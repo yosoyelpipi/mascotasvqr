6cc661dceec82246bc4d7e1161fd98a159e10d5a
yosoyelpipi <lcondori@gmail.com>
2014-12-13 00:47:31 -0300

    Ini

---
+++ js/mipropiojava.js
@@ -0,0 +1,130 @@
+
+
+function onBodyLoad(){
+	document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+function onDeviceReady(){
+	$("#resultado").html("Dispositivo Android detectado.");
+
+	//navigator.accelerometer.getCurrentAcceleration(onInfo, onError);
+    checkConnection();
+    document.addEventListener("backbutton", onBackKeyDown, false);
+
+}
+
+
+// Handle the menu button
+//
+function onMenuKeyDown() {
+	alert('Menu button pressed.');    
+
+    }
+
+
+function onBackKeyDown() {
+            if( confirm("Realmente desea salir de la aplicación?") )
+            {
+                  navigator.app.exitApp();
+            }
+       } 
+
+/*
+*
+red
+*
+*/
+
+        function checkConnection() {
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cell generic connection';
+            states[Connection.NONE]     = 'No network connection';
+
+            alert('Tu tipo de conexión es: ' + states[networkState]);
+        }
+
+/*
+*
+audio
+*
+*/
+
+// Audio player
+        //
+        var my_media = null;
+        var mediaTimer = null;
+
+        // Play audio
+        //
+        function playAudio(src) {
+            // Create Media object from src
+            my_media = new Media(src, onSuccess, onError);
+
+            // Play audio
+            my_media.play();
+
+            // Update my_media position every second
+            if (mediaTimer == null) {
+                mediaTimer = setInterval(function() {
+                    // get my_media position
+                    my_media.getCurrentPosition(
+                        // success callback
+                        function(position) {
+                            if (position > -1) {
+                                setAudioPosition((position) + " sec");
+                            }
+                        },
+                        // error callback
+                        function(e) {
+                            console.log("Error getting pos=" + e);
+                            setAudioPosition("Error: " + e);
+                        }
+                    );
+                }, 1000);
+            }
+        }
+
+        // Pause audio
+        //
+        function pauseAudio() {
+            if (my_media) {
+                my_media.pause();
+            }
+        }
+
+        // Stop audio
+        //
+        function stopAudio() {
+            if (my_media) {
+                my_media.stop();
+            }
+            clearInterval(mediaTimer);
+            mediaTimer = null;
+        }
+
+        // onSuccess Callback
+        //
+        function onSuccess() {
+            console.log("playAudio():Audio Success");
+        }
+
+        // onError Callback
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                  'message: ' + error.message + '\n');
+        }
+
+        // Set audio position
+        //
+        function setAudioPosition(position) {
+            document.getElementById('audio_position').innerHTML = position;
+        }
